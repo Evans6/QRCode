0a146f953bc7f2fcef972640e23248abb7a2d920
Evans6 <229414698@qq.com>
2018-12-21 20:45:01 +0800

    first commit

---
+++ src/main/java/QRCode/test1.java
@@ -0,0 +1,10 @@
+package QRCode;
+
+public class test1 {
+	public static void main(String[] args) throws Exception {
+		System.out.println(222);
+		QRCodeUtil.encode("aaa", "C:\\Users\\Administrator\\Desktop\\新建文件夹\\logo.png", "C:\\Users\\Administrator\\Desktop\\新建文件夹", true,"QRCode01");
+		System.out.println("111");
+		
+	}
+}
